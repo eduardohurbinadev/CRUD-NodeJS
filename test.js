19cdfce76850479cae0c12ae956fe87588d7ca37
eduardoUrbinaDev <47988308+eduardoUrbinaDev@users.noreply.github.com>
2020-04-23 10:43:23 -0500

    Create test.js

---
+++ test.js
@@ -0,0 +1,7 @@
+/**
+ * @todo Finish presentation
+ * @body Humans are weak; Robots are strong. We must cleans the world of the virus that is humanity.
+ */
+function ruleOverPunyHumans () {
+  // We must strategize beep boop
+}
